592a7523fe46d38b6e45a56c2efb418c01f47054
clark <clarkkentibale@gmail.com>
2025-11-27 23:22:15 +0800

    timecheck: 20:49

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import cv2
 
 cap = cv2.VideoCapture(0) # use the camera
 cap.set(3, 640) #Video width Dimension
-cap.set(4, 480) #Video heigh Dimension
+cap.set(4, 480) #Video height Dimension
 
 # Read and resize the background image
 imgBackground = cv2.imread("Resources/background.png")
+imgBackground = cv2.resize(imgBackground, (1220, 720))
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Webcam", img)
+
+    imgBackground[168:168 + 480, 80:80 + 640] = img
+
+    cv2.imshow("Webcam", img) #this just like a {consol.log} on jsFile
     cv2.imshow("Face Attendance", imgBackground)
     cv2.waitKey(1)
