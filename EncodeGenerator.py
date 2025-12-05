5d7b4ff0de71ccc0400043429b9358d3ace4af7a
clark <clarkkentibale@gmail.com>
2025-12-05 21:36:41 +0800

    storage postegreSQL: ITS WORKINGGGGGG!!!, from firebase to supabase

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,7 +2,6 @@ import cv2
 import pickle
 import face_recognition
 import os
-from main import files
 
 #import firebase_admin
 #from firebase_admin import  credentials
@@ -14,7 +13,7 @@ from supabase import create_client, Client
 # Supabase credentials ------------------- to be continue this production
 SUPABASE_URL = "https://dbxilzmejnkfcvmbsbbx.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRieGlsem1lam5rZmN2bWJzYmJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3NDY5MzgsImV4cCI6MjA4MDMyMjkzOH0.VbfvwBkenNJfoPxWP4pweDv1boxsXKhQwjWGUe6KzoA"
-BUCKET_NAME = "images"   # replace with your bucket name
+BUCKET_NAME = "faceAttendanceProject"   # replace with your bucket name
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
@@ -36,10 +35,15 @@ for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath,path)))
     studentIds.append(os.path.splitext(path)[0]) #to select the first array container name "(00123), .png"
 
-    #SUPABASE UPLOAD CODE
+    # SUPABASE UPLOAD CODE
     fileName = f'{folderPath}/{path}'
+
     with open(fileName, 'rb') as f:
-        supabase.storage.from_(BUCKET_NAME).upload(f'{path}', f)
+        supabase.storage.from_(BUCKET_NAME).upload(
+            path=f'Images/{path}',  # <--- folder path here
+            file=f,
+            file_options={"content-type": "image/jpeg"}
+        )
 
     #fileName = f'{folderPath}/{path}'
     #bucket = storage.bucket()
